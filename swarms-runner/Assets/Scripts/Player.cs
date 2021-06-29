a760cd8496071980477bba3112662aa73522dacb
metrinandra <48483843+metrinandra@users.noreply.github.com>
2021-06-29 09:45:04 +0300

    Player script

--- swarms-runner/Assets/Scripts/Player.cs
+++ swarms-runner/Assets/Scripts/Player.cs
@@ -6,15 +6,26 @@ public class Player : MonoBehaviour
 {
     
     public float playerSpeed;
+    public Rigidbody2D rb;
+    public Vector2 playerDirection;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       float directionY = Input.GetAxisRaw("Vertical"); 
+       playerDirection = new Vector2(0,directionY).normalized;
+
     }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(0, playerDirection.y * playerSpeed);
+    }
+
 }
