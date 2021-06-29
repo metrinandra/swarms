1d8194aaef233ef0225ed0d6dac9b924e512e357
metrinandra <48483843+metrinandra@users.noreply.github.com>
2021-06-29 12:08:46 +0300

    Fixing problems

--- swarms-runner/Assets/Scripts/Obstacle.cs
+++ swarms-runner/Assets/Scripts/Obstacle.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.tag == "Border"){
             Destroy(this.gameObject);
+        }
+
+        else if(collision.tag == "Player"){
+            Destroy(player.gameObject);
         }
     }
 }
