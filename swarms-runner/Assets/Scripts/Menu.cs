89660ad80e33c4f5a8969814ab6bd1bde22ffcc3
metrinandra <48483843+metrinandra@users.noreply.github.com>
2021-06-30 23:22:28 +0300

    PrintText corutine

--- swarms-runner/Assets/Scripts/Menu.cs
+++ swarms-runner/Assets/Scripts/Menu.cs
@@ -9,16 +9,16 @@ public class Menu : MonoBehaviour
 
     void Start(){
         string[] texts = {"Lockdown game", "avoid bat swards", "and stay negative", "hit PLAY button to start"};
-       PrintText(texts, 1.0f);
+        StartCoroutine(PrintText(texts, 1.0f));
    }
     public void PlayGame (){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
 }
 
-    IEnumerator PrintText(string[]texts, float delay){
+    private IEnumerator PrintText(string[]texts, float delay){
         foreach (string txt in texts){
-            print(txt);
             yield return new WaitForSeconds (delay);
+            print(txt);
         }
 
     }
