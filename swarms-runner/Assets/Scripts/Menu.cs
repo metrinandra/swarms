44a54208bcf1150c73d463b140c62211a1a00b08
metrinandra <48483843+metrinandra@users.noreply.github.com>
2021-06-30 22:44:39 +0300

    Last try

--- swarms-runner/Assets/Scripts/Menu.cs
+++ swarms-runner/Assets/Scripts/Menu.cs
@@ -5,7 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
+    
+
+    void Start(){
+        string[] texts = {"Lockdown game", "avoid bat swards", "and stay negative", "hit PLAY button to start"};
+       PrintText(texts, 1.0f);
+   }
     public void PlayGame (){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
 }
+
+    IEnumerator PrintText(string[]texts, float delay){
+        foreach (string txt in texts){
+            print(txt);
+            yield return new WaitForSeconds (delay);
+        }
+
+    }
 }
